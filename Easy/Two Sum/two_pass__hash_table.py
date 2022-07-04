04fd590cc9ef7394e1a80e6ccfd1d78809f1f6f4
Shanchien Lee <102730245+ShanchienLee@users.noreply.github.com>
2022-07-04 11:40:32 +0800

    Update two_pass__hash_table.py

--- Easy/Two Sum/two_pass__hash_table.py
+++ Easy/Two Sum/two_pass__hash_table.py
@@ -8,9 +8,3 @@ class Solution:
             if a in hashmap and hashmap[a] != i:
                 return[i, hashmap[a]]
             
-#   先建立一個hashmap
-#   用迴圈將value, index存進hashmap
-#   用迴圈找出殘差a
-#   以a值為key，於hashmap中帶出hashmap[a]值(實為array index)
-#   條件hashmap[a]不等於i
-#   回傳[i, hasmap[a]]
